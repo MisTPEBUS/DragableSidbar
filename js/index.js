5de5846c208cb0b038bfa8140655b134d6c5892c
vue2023W2 <lobinda123@gmail.com>
2024-03-19 08:53:21 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,33 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+const sidbarMaxWidth = 200;
+const maxPercent = 100;
+const minPercent = 10;
+const defaultPercent = 20;
+
+
+createApp({
+  data: () => ({
+    dividerPosition: 0,
+
+  }),
+  methods: {
+    handleDragging(e) {
+      const percentage = (e.pageX / window.innerWidth) * 100
+
+      if (e.pageX >= sidbarMaxWidth && percentage >= minPercent && percentage <= maxPercent) {
+        this.dividerPosition = percentage.toFixed(2)
+      }
+    },
+    startDragging() {
+      document.addEventListener('mousemove', this.handleDragging)
+    },
+    endDragging() {
+      document.removeEventListener('mousemove', this.handleDragging)
+    },
+  },
+  mounted() {
+    this.dividerPosition = defaultPercent;
+    this.dividerPosition = (sidbarMaxWidth / window.innerWidth <= 20) ? 20 : (sidbarMaxWidth / window.innerWidth);
+
+  }
+}).mount('#app');
